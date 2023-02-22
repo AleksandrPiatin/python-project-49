67d7c9a88a6310937cb0451b0358c78738cf1a25
AleksandrPiatin <aspyatin1508@gmail.com>
2023-02-22 22:17:01 +0300

    add new game gcd.py and new script brain_gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,17 @@
+import random
+
+
+TASK = 'Find the greatest common divisor of given numbers.'
+
+
+def game_task():
+    number_1 = random.randint(1, 100)
+    number_2 = random.randint(1, 100)
+    question = f'{number_1} {number_2}'
+    while number_1 != 0 and number_2 != 0:
+        if number_1 > number_2:
+            number_1 = number_1 % number_2
+        else:
+            number_2 = number_2 % number_1
+        correct_answer = str(number_1 + number_2)
+    return question, correct_answer
