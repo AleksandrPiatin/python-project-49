52bf651c7a373a5369df6de2d14d20e68f241719
AleksandrPiatin <aspyatin1508@gmail.com>
2023-03-04 15:46:03 +0300

    add new game prime.py and new script brain_prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,14 @@
+import random
+
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def game_task():
+    question = random.randint(1, 100)
+    a = random.randint(1, 100)
+    if (a % question != 0) and (question > 1) and ((a ** (question -1) - 1) % question == 0):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return question, correct_answer
