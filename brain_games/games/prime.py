c34e5c253d97ef8bf1f69bd7af6528aa731bface
AleksandrPiatin <aspyatin1508@gmail.com>
2023-03-06 21:08:38 +0300

    fix brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,10 +5,10 @@ TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def game_task():
-    question = random.randint(1, 100)
-    a = random.randint(1, 100)
-    if (a % question != 0) and (question > 1) and \
-            ((a ** (question - 1) - 1) % question == 0):
+    question = random.randint(2, 100)
+    a = 2
+    '''take the number "a" to check for simplicity'''
+    if ((a ** (question - 1) - 1) % question == 0):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
